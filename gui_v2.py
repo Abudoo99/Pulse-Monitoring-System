8ccec8104cb57d49102dd99287f606c728ea2480
Jessie Kao <114864225+JessieYchKao@users.noreply.github.com>
2023-11-30 16:00:57 -0800

    Implement HRSTD

--- gui_v2.py
+++ gui_v2.py
@@ -30,6 +30,7 @@ class CalcBPM(QObject):
         self.running = False
         self.bpm = -1
         self.rmssd = -1
+        self.hrstd = -1
         self.time_interal_beat = [] # For calculating RMSSD
 
     def run(self):
@@ -42,7 +43,7 @@ class CalcBPM(QObject):
                 prev_beat = 0 # For calculating RMSSD
                 while cur_time - start_time < 10:
                     # TODO: get the read data through Bluetooth
-                    data = random.randrange(50, 180)
+                    data = random.randrange(0, 255)
                     global client_socket
                     #data = client_socket.recv(size)
                     if data:
@@ -51,13 +52,13 @@ class CalcBPM(QObject):
                         self.bpmList = np.append(self.bpmList, signal)
 
                         if signal > self.threshold:
-                            count += 1
                             beat = time.time()
                             print("beat!!")
 
                             if count > 0:
                                 self.time_interval_beat.append(round(abs((beat - prev_beat)*1000),2))
                             prev_beat = beat
+                            count += 1
 
 
                     time.sleep(0.15) # TODO: Remove
@@ -65,11 +66,11 @@ class CalcBPM(QObject):
 
                 # Update BPM
                 self.bpm = count * 6
+                print("Time:\n", self.time_interval_beat)
                 print("BPM: %d" % self.bpm)
                 # Update RMSSD
-                self.rmssd = self.calculate_rms()
+                self.rmssd, self.hrstd = self.calculate_rms()
                 print("RMSSD: ", self.rmssd)
-
                 start_time = time.time()
 
 
@@ -80,13 +81,14 @@ class CalcBPM(QObject):
             rms_arr.append(round(abs(self.time_interval_beat[i] - self.time_interval_beat[i-1]),2))
 
         # Calculating RMSSD value
-        rms = math.sqrt(np.square(rms_arr).mean())
+        rms = round(math.sqrt(np.square(rms_arr).mean()),2)
+        hrstd = round(stdev(rms_arr),2)
+        
         #print("Time interval between successive beats for the past ten seconds\n",time_interval_beat)
         #print("RMS array: ", rms_arr)
 
-        return rms
-
-
+        return rms, hrstd
+    
 class BPMPage(QWidget):
     def __init__(self, parent=None):
         super(BPMPage, self).__init__(parent)
@@ -223,6 +225,7 @@ class BPMPage(QWidget):
         self.curve.setData(np.arange(0, self.calcBPM.bpmList.size, 1), self.calcBPM.bpmList)
         self.bpmValue.setText((str(self.calcBPM.bpm) if self.calcBPM.bpm >= 0 else "--"))
         self.ipmValue.setText((str(self.calcBPM.bpm) if self.calcBPM.bpm >= 0 else "--"))
+        self.hrstdValue.setText((str(self.calcBPM.hrstd) if self.calcBPM.hrstd >= 0 else "--"))
         self.rmssdValue.setText((str(self.calcBPM.rmssd) if self.calcBPM.rmssd >= 0 else "--"))
 
 
